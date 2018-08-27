953af11a7a2656dcf11087b1cb0cc5e10e486ddf
sxhdroid <sxh_droid@163.com>
2018-08-27 15:21:36 +0800

    人脸识别

--- chapter5/face_recognition.py
+++ chapter5/face_recognition.py
@@ -38,10 +38,38 @@ def read_image(faces, sz=None):
     return [X, y]
 
 
-def face_rec():
+def face_rec(bylocalvideo=False, localvideo=None):
     names = ['xsh', 'dp', 'wj']
     [X, y] = read_image('./data/xsh.csv')
-    
+
+    model = cv2.face.EigenFaceRecognizer_create()
+    model.train(np.asarray(X), np.asarray(y))  # Lable 只能是int类型
+    if bylocalvideo:
+        video = cv2.VideoCapture(localvideo)
+    else:
+        video = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    success, frame = video.read()
+    while success:
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)  # 检测人脸
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 转化为灰度图
+            roi_gray = gray[y:y+h, x:x+w]
+            try:
+                roi = cv2.resize(roi_gray, (200, 200), interpolation=cv2.INTER_LINEAR)
+                params = model.predict(roi)
+                print('Label: %s, Confidence: %.2f' % (params[0], params[1]))
+                cv2.putText(img, names[params[0]], (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+            except:
+                continue
+        cv2.imshow('camera', frame)
+        success, frame = video.read()
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    face_rec()
+    face_rec(bylocalvideo=True, localvideo='./data/alive.mp4')
